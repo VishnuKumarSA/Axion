5052ce9fdda16307c360a33ee54bf32cb397e059
Vishnu <savishnukumar@gmail.com>
2026-01-02 20:11:36 +0530

    missing code added

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -22,4 +22,9 @@ class Role extends Model
     {
         return $this->belongsToMany(User::class, 'user_roles');
     }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'role_permissions');
+    }
 }
